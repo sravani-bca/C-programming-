fe0621209f9b4189872a61b8ab9f7cd032d5e09c
Sravani <s8858537@gmail.com>
2026-08-11 21:54:12 +0530

    Day 9: Practiced sizeof operator in C

---
+++ day 9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int i;
+  float f;
+  char c;
+  double d;
+  long double Id;
+  printf("sizeof integer = %d\n",sizeof(i));
+  printf("sizeof float = %d\n",sizeof(f));
+  printf("sizeof char = %d\n",sizeof(c));
+  printf("sizeof double = %d\n",sizeof(d));
+  printf("sizeof Long double = %d\n",sizeof(Id));
+  return 0;
+}
+  
