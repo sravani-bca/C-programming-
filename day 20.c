36193714d0c2a19c0d22c44937faa92fa668a503
Sravani <s8858537@gmail.com>
2026-08-25 08:55:21 +0530

    Add program to find sum of digits

---
+++ day 20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, sum = 0;
+
+    printf("Enter the number: ");
+    scanf("%d", &number);
+
+    while (number != 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+
+    printf("Sum of digits is %d", sum);
+
+    return 0;
+}
