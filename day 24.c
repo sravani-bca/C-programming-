0521ca3d240abe47fa35ad09372b2c05e24d7524
Sravani <s8858537@gmail.com>
2026-08-28 21:07:31 +0530

    Added program to extract digits of an integer

---
+++ day 24.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int num, temp, factor = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while(temp)
+    {
+        temp = temp / 10;
+        factor = factor * 10;
+    }
+
+    printf("Each digits of given number are: ");
+
+    while(factor > 1)
+    {
+        factor = factor / 10;
+        printf("%d ", num / factor);
+        num = num % factor;
+    }
+
+    getch();
+    return 0;
+}
