6d768436c497d64dd1b898d9f5e01b2136fcdc07
Sravani <s8858537@gmail.com>
2026-09-22 08:47:42 +0530

    Added C program using tolower() function

---
+++ day 43.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    printf("Lower case of A is %c\n", tolower('A'));
+    printf("Lower case of 9 is %c\n", tolower('9'));
+    printf("Lower case of g is %c\n", tolower('g'));
+
+    return 0;
+}
